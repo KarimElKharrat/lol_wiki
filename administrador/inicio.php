793c53014baef731db88f67323d2eaeb218978fd
KarimElKharrat <113608883+KarimElKharrat@users.noreply.github.com>
2023-05-29 20:55:06 +0100

    inicio admin

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -8,14 +8,14 @@ File::includeTemplateFile('adminHeader.php');
 
 <div class="col-md-12">
 
-    <div class="jumbotron">
-        <h1 class="display-3">Inicio</h1>
-        <p class="lead">Jumbo helper text</p>
+    <div class="jumbotron text-center">
+        <h1 class="display-3">Bienvenido al panel de administrador</h1>
+        <!-- <p class="lead">Jumbo helper text</p>
         <hr class="my-2">
         <p>More info</p>
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-        </p>
+        </p> -->
     </div>
 
 </div>
